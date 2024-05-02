8ecf65a65eeae22b9adacdec262701962a24bf4c
Jakob <jakob.d.neuhauser@gmail.com>
2024-05-02 10:29:28 +0200

    Initial commit

---
+++ eis_lib/eis.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jakob on 02.05.24.
+//
+
+#include "eis.h"
---
+++ eis_lib/eis.h
@@ -0,0 +1,14 @@
+//
+// Created by Jakob on 02.05.24.
+//
+
+#ifndef EIS_EIS_H
+#define EIS_EIS_H
+
+
+class eis {
+
+};
+
+
+#endif //EIS_EIS_H
