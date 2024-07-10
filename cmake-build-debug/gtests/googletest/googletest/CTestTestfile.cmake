5625fc825964f58f908c5b0d5ac81f7cac2a8504
Jakob <jakob.d.neuhauser@gmail.com>
2024-07-10 16:28:34 +0200

    gitignore added

---
+++ cmake-build-debug/gtests/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/h1v3r/CLionProjects/eis/gtests/googletest/googletest
+# Build directory: /Users/h1v3r/CLionProjects/eis/cmake-build-debug/gtests/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
