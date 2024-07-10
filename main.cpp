24e46153648d02656ed5121b7a09dda2835f7621
Jakob <jakob.d.neuhauser@gmail.com>
2024-07-10 17:40:23 +0200

    main file excluded from cmake. Instructions added to reinclude it

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+// INFO
+// --------------------------------------------------------------------------------
+// To use the main file uncomment the two marked lines in the CMakeLists.txt file.
+// --------------------------------------------------------------------------------
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
